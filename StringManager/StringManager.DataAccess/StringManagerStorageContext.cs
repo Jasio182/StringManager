cb7d70e4adb0ad1a033f072e3e3d22b170f0b818
Jan Mokracki <jan.mokracki@wp.pl>
2021-08-28 10:52:10 +0200

    Added connection string

---
+++ StringManager/StringManager.DataAccess/StringManagerStorageContext.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using StringManager.DataAccess.Entities;
+
+namespace StringManager.DataAccess
+{
+    public class StringManagerStorageContext : DbContext
+    {
+        public StringManagerStorageContext(DbContextOptions<StringManagerStorageContext> opt) : base(opt) { }
+
+        public DbSet<InstalledString> InstalledStrings { get; set; }
+
+        public DbSet<Instrument> Instruments { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<MyInstrument> MyInstruments { get; set; }
+
+        public DbSet<String> Strings { get; set; }
+
+        public DbSet<StringInSet> StringsInSets { get; set; }
+
+        public DbSet<StringsSet> StringsSets { get; set; }
+
+        public DbSet<Tone> Tones { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ StringManager/StringManager.DataAccess/StringManagerStorageContextFactory.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace StringManager.DataAccess
+{
+    public class StringManagerStorageContextFactory : IDesignTimeDbContextFactory<StringManagerStorageContext>
+    {
+        public StringManagerStorageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StringManagerStorageContext>();
+            optionsBuilder.UseSqlServer("Data Source=LOCALHOST.\\SQLEXPRESS;Initial Catalog=StringManager;Integrated Security=True");
+            return new StringManagerStorageContext(optionsBuilder.Options);
+        }
+    }
+}
